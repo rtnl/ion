3b9442f94c9ebf27005b9c22d816de8f46f9b7da
snavy <1337snavy@gmail.com>
2025-01-26 08:44:31 +0100

    enable warnings to build script

--- build/install.sh
+++ build/install.sh
@@ -2,6 +2,6 @@
 export WORKDIR=$(mktemp -d)
 
 set -e
-cc -shared -o "${WORKDIR}/libion.so" ./src/*.c
+cc -Wall -Wextra -shared -o "${WORKDIR}/libion.so" ./src/*.c
 sudo install "${WORKDIR}/libion.so" /usr/local/include
 sudo install ./src/ion.h /usr/local/include

72df5437105867a66ad5d9b7439f2862c0a2f3cb
snavy <1337snavy@gmail.com>
2025-03-03 19:54:07 +0100

    add valgrind test

--- build/test.sh
+++ build/test.sh
@@ -3,4 +3,13 @@ export WORKDIR=$(mktemp -d)
 
 set -e
 cc -DTEST -lcriterion -o "${WORKDIR}/libion_test" ./src/*.c
+
+echo "running tests"
 "${WORKDIR}/libion_test" --verbose
+
+if [ -x "$(command -v valgrind)" ]; then
+    echo "valgrind found"
+    valgrind "${WORKDIR}/libion_test"
+else
+    echo "valgrind not found"
+fi

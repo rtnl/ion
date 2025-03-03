d432d3015e49931b92b2fe559b04561a08506548
snavy <1337snavy@gmail.com>
2025-03-03 19:39:41 +0100

    buffer encode nested test

--- src/buffer_test.c
+++ src/buffer_test.c
@@ -196,4 +196,120 @@ Test(buffer, buffer_encode_struct) {
   cr_expect(memcmp(data, data_expected, 23) == 0);
 }
 
+Test(buffer, buffer_encode_nested) {
+  t_ion_buffer *buffer;
+  t_ion_result_code result;
+
+  buffer = ion_buffer_new();
+  cr_expect(buffer != NULL);
+
+  const uint32_t t = 1024;
+
+  for (uint32_t x = 0; x < t; x++) {
+    result = ion_buffer_io_write_list_open(buffer);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_arr_open(buffer, U8);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u8(buffer, 'X');
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u8(buffer, 'Y');
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u8(buffer, 'Z');
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_arr_close(buffer);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u0(buffer);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u8(buffer, UINT8_MAX);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u16(buffer, UINT16_MAX);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u32(buffer, UINT32_MAX);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_write_u64(buffer, UINT64_MAX);
+    cr_expect(result == RESULT_OK);
+  }
+
+  for (uint32_t x = 0; x < t; x++) {
+    result = ion_buffer_io_write_list_close(buffer);
+    cr_expect(result == RESULT_OK);
+  }
+
+  for (uint32_t x = 0; x < t; x++) {
+    uint8_t len = 0;
+    result = ion_buffer_io_read_list_open(buffer, &len);
+    cr_expect(result == RESULT_OK);
+
+    if (x == (t - 1)) {
+      cr_expect(len == 6);
+    } else {
+      cr_expect(len == 7);
+    }
+
+    t_ion_object_kind item = 0;
+    result = ion_buffer_io_read_arr_open(buffer, &item, &len);
+    cr_expect(result == RESULT_OK);
+    cr_expect(item == U8);
+    cr_expect(len == 3);
+
+    uint8_t value_u8 = 0;
+    uint16_t value_u16 = 0;
+    uint32_t value_u32 = 0;
+    uint64_t value_u64 = 0;
+
+    result = ion_buffer_io_read_u8(buffer, &value_u8);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u8 == 'X');
+
+    result = ion_buffer_io_read_u8(buffer, &value_u8);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u8 == 'Y');
+
+    result = ion_buffer_io_read_u8(buffer, &value_u8);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u8 == 'Z');
+
+    result = ion_buffer_io_read_arr_close(buffer);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_read_u0(buffer);
+    cr_expect(result == RESULT_OK);
+
+    result = ion_buffer_io_read_u8(buffer, &value_u8);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u8 == UINT8_MAX);
+
+    result = ion_buffer_io_read_u16(buffer, &value_u16);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u16 == UINT16_MAX);
+
+    result = ion_buffer_io_read_u32(buffer, &value_u32);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u32 == UINT32_MAX);
+
+    result = ion_buffer_io_read_u64(buffer, &value_u64);
+    cr_expect(result == RESULT_OK);
+    cr_expect(value_u64 == UINT64_MAX);
+  }
+
+  for (uint32_t x = 0; x < t; x++) {
+    result = ion_buffer_io_read_list_close(buffer);
+    cr_expect(result == RESULT_OK);
+  }
+
+  cr_expect(buffer->body->curr_r == buffer->body->curr_w);
+
+  ion_buffer_free(buffer);
+}
+
 #endif

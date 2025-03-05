409b5c0f5cca38a10250b54e54b9df3cd0c146bc
snavy <1337snavy@gmail.com>
2025-03-05 21:15:57 +0100

    fix io peek open using read

--- src/buffer_state.c
+++ src/buffer_state.c
@@ -422,7 +422,7 @@ t_ion_result_code ion_buffer_state_io_peek_open(t_ion_buffer *self,
   }
 
   case LIST: {
-    result = ion_buffer_io_read_data(self, U8, len);
+    result = ion_buffer_io_peek_data(self, U8, len);
     if (result != RESULT_OK)
       return result;
 
